086c949841a5ca4006755aae24f0309e558359a1
Alex Dudyak <adudyak@geeksforless.net>
2018-01-02 18:38:15 +0200

    minor fixes

--- src/CheckSite.java
+++ src/CheckSite.java
@@ -1,9 +1,10 @@
 import java.io.IOException;
 import java.net.*;
+import java.time.LocalDateTime;
 
 public class CheckSite implements Runnable {
-    private static int taskCount = 0;
-    int attemptCount = taskCount++;
+    private static int thread = 0;
+    int threadCount = thread++;
     URL myURL = new URL("http://google.com");
     int attemptsAmount = 3;
     int intervalBetweenAttempts = 1000; //ms
@@ -14,11 +15,10 @@ public class CheckSite implements Runnable {
             connection = (HttpURLConnection) myURL.openConnection();
             connection.setRequestMethod("HEAD");
             for (int i = 0; ; i++) {
-                System.out.println("Attempt #" + i + ". " + Thread.currentThread().getName() + ". " + myURL + " Response code: " + connection.getResponseCode());
+                System.out.println("Attempt-" + i + " " + Thread.currentThread().getName() + " "+  LocalDateTime.now() + " Response code " + connection.getResponseCode());
                 if (i == attemptsAmount) break;
                 Thread.sleep(intervalBetweenAttempts);
             }
-            Thread.yield();
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (ProtocolException e) {
--- src/ThreadsRun.java
+++ src/ThreadsRun.java
@@ -5,8 +5,8 @@ public class ThreadsRun {
         Thread t0 = new Thread(new CheckSite());
         Thread t1 = new Thread(new CheckSite());
         Thread t2 = new Thread(new CheckSite());
-        t0.setPriority(5);
-        t1.setPriority(6);
+        t0.setPriority(2);
+        t1.setPriority(8);
         t2.setPriority(10);
         t0.start();
         t1.start();
